355998b66158f8c7aff8b5864bab99de0fcd353b
StarZihack <starzihack@gmail.com>
2024-09-28 11:30:01 +1000

    Initial Commit

---
+++ app/src/main/java/com/example/friendzy/activities/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.friendzy.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.friendzy.R;
+import com.example.friendzy.databinding.ActivitySignInBinding;
+import com.example.friendzy.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
